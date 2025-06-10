1122643baf7383554f3e8d639ac9eae3254576c8
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-10 18:50:10 +0900

    Fix FileService base address

--- BlazorIW.Client/Services/FileService.cs
+++ BlazorIW.Client/Services/FileService.cs
@@ -1,13 +1,20 @@
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorIW.Client.Services;
 
-public class FileService(HttpClient httpClient)
+public class FileService(HttpClient httpClient, NavigationManager navigationManager)
 {
     private readonly HttpClient _httpClient = httpClient;
+    private readonly NavigationManager _navigationManager = navigationManager;
 
     public async Task<IEnumerable<string>> GetFilesAsync()
     {
+        if (_httpClient.BaseAddress is null)
+        {
+            _httpClient.BaseAddress = new Uri(_navigationManager.BaseUri);
+        }
+
         return await _httpClient.GetFromJsonAsync<IEnumerable<string>>("api/files")
             ?? Enumerable.Empty<string>();
     }
