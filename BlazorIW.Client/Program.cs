6fb26c18098588989fa46d28502923f5bc360d43
Yasuaki Kudo <yasu@tng.coop>
2025-06-10 11:00:43 +0900

    bad

--- BlazorIW.Client/Program.cs
+++ BlazorIW.Client/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorIW.Client.Pages;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+
+// Register Counter component as custom element <my-counter>
+builder.RootComponents.RegisterCustomElement<Counter>("my-counter");
 
 await builder.Build().RunAsync();
