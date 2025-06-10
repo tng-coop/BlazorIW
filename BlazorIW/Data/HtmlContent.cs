4b47a4cd21121b53882a0df219c8f46240c14676
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-10 17:54:22 +0900

    Add revision support for HTML content

--- BlazorIW/Data/HtmlContent.cs
+++ BlazorIW/Data/HtmlContent.cs
@@ -1,7 +1,12 @@
 namespace BlazorIW.Data;
 
-public class HtmlContent : IContent, IHtmlContent
+public class HtmlContent : IContent
 {
     public Guid Id { get; init; } = Guid.NewGuid();
+}
+
+public class HtmlContentRevision : HtmlContent, IHtmlContent
+{
+    public int Revision { get; set; } = 1;
     public string Html { get; set; } = string.Empty;
 }
