f0f9f2feb174dd10c0efa15bfbc5e68339993775
Yasuaki Kudo <yasu@tng.coop>
2025-06-10 11:29:46 +0900

    switch to postgres

--- BlazorIW/Program.cs
+++ BlazorIW/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddAuthorization();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
