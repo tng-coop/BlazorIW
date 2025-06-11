ca3e4b8e66ef74ec1405b3b4b2fe55ae07cacf30
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-11 09:48:40 +0900

    Add postal code service and seed data

---
+++ BlazorIW/Data/PostalCode.cs
@@ -0,0 +1,8 @@
+namespace BlazorIW.Data;
+
+public class PostalCode
+{
+    public string Zipcode { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
